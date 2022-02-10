1fa66b5188028af8fe912664d32408ec18ad8f9c
maroua199525 <2646@holbertonschool.com>
2022-02-10 16:55:21 +0100

    add task 1 function add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+""" module contain a function add"""
+
+def add(a: float, b: float) -> float:
+    """a function  that takes a float a and a float b as arguments
+        and returns their sum as a float"""
+    return ( a + b)
+
+if __name__ == "__main__":
+    add()
